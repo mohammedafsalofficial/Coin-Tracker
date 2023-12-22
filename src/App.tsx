332dfa489fbeb66c900334c940ea425fd9557bc8
Mohammed Afsal <mohammedafsaltut@gmail.com>
2023-12-22 18:04:13 +0530

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
 
 import "./App.css";
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <div className="min-h-screen flex justify-center items-start py-20">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <h1 className="w-96 text-3xl font-bold my-5">Coin Tracker</h1>
+    </>
+  );
+};
+
+export default Header;
